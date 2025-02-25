50427b7f6e356a9425cfdf25d0bcccba56d16cfd
Pranjallpandey1504 <pranjal1504pandey@gmail.com>
2025-02-25 14:17:49 +0530

    moved index.html

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model("users", UserSchema);
 
 // Routes for Pages
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "views", "about.html")));
 app.get("/contact", (req, res) => res.sendFile(path.join(__dirname, "views", "contact.html")));
 app.get("/profile", (req, res) => res.sendFile(path.join(__dirname, "views", "profile.html")));
